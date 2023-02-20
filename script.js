a1f8efae889a20cfaca7a046911c4d38bb1b727a
umeshchandra24 <120272310+umeshchandra24@users.noreply.github.com>
2023-02-20 22:25:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ function calculateMinCost() {
 	var cost=0;
 	while(arr.length>1){
 		var res = Number(arr[0]) + Number(arr[1]);
+		arr.splice(0,2);
 		arr.push(res);
 		 cost += res;
-		arr.splice(0,2);
 		arr.sort(function (a,b){return a-b;})
 	}  
 	document.getElementById("result").textContent = cost;
